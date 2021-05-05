18482777f3f80571d3f261c67c402884b7177969
Bruno <silva.bruno@outlook.com>
2021-05-05 15:23:19 -0300

    Autenticando token de usuarios

--- src/business/ImageBusiness.ts
+++ src/business/ImageBusiness.ts
@@ -1,6 +1,7 @@
 import { ImageDatabase } from "../data/ImageDatabase";
 import { UserDatabase } from "../data/UserDatabase";
 import { CustomError } from "../errors/CustomError";
+import { CollectionInputDTO } from "../model/Collections";
 import { allImagesDTO, ImageInputDTO, Images } from "../model/Images";
 import { Tags } from "../model/Tags";
 import { Authenticator } from "../services/Authenticator";
@@ -78,6 +79,14 @@ export class ImageBusiness {
                 throw new CustomError(422, "Sorry!You must be 'login' first")
             }
 
+            const authToken = this.authenticator.getData(token)
+
+            const authUser = this.userDatabase.getUserById(authToken.id)
+
+            if (!authUser) {
+                throw new CustomError(404, "Sorry! User not found")
+            }
+
             const results = await this.imageDatabase.getAllImages()
 
             if (!results) {
@@ -103,16 +112,47 @@ export class ImageBusiness {
                 throw new CustomError(422, "Please check 'id' were filled")
             }
 
+            const authToken = this.authenticator.getData(token)
+
+            const authUser = this.userDatabase.getUserById(authToken.id)
+
+            if (!authUser) {
+                throw new CustomError(404, "Sorry! User not found")
+            }
+
             const details = await this.imageDatabase.getImageDetails(id)
             const tags = await this.imageDatabase.getImageTags(id)
 
             if (!details && !tags) {
                 throw new CustomError(404, "Sorry! Try again in 1 minute")
-            }           
+            }
 
-            
+            return { details, tags }
 
-            return {details, tags}
+        } catch (error) {
+            throw new CustomError(error.statusCode, error.message)
+        }
+    }
+
+    public async createCollection(token: string, input: CollectionInputDTO): Promise<void> {
+
+        try {
+
+            if (!token) {
+                throw new CustomError(422, "Sorry!You must be 'login' first")
+            }
+
+            if (!input.title || !input.subtitle) {
+                throw new CustomError(422, "Please check 'title', 'subtitle',  were filled")
+            }
+
+            const authToken = this.authenticator.getData(token)
+
+            const authUser = this.userDatabase.getUserById(authToken.id)
+
+            if (!authUser) {
+                throw new CustomError(404, "Sorry! User not found")
+            }
 
         } catch (error) {
             throw new CustomError(error.statusCode, error.message)
